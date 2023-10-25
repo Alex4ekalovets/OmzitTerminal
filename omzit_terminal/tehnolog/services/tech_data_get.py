b0bd8818671e1d362bb973336d30a8ecf7392e7e
Чекаловец А.В. <user-357@omep.net.ru>
2023-10-25 17:33:35 +0600

    started file upload form in tehnolog

--- omzit_terminal/tehnolog/services/tech_data_get.py
+++ omzit_terminal/tehnolog/services/tech_data_get.py
@@ -19,7 +19,9 @@ def tech_data_get(model_order_query: str, exel_file: str, excel_list: str):
     :return: None
     """
     # имя модели в базу
-    model_name, order = model_order_query.split('-')
+    model_name = model_order_query[model_order_query.find("_") + 1:]
+    # номер заказа в базу модели в базу
+    order = model_order_query[:model_order_query.find("_")]
 
     # Получение списка данных
     common_data = {
@@ -35,6 +37,7 @@ def tech_data_get(model_order_query: str, exel_file: str, excel_list: str):
         tasks = [ShiftTask(**data) for data in data_list]
         ShiftTask.objects.bulk_create(tasks)
 
+    is_uploaded = True
     if not shift_tasks.exists():
         create_all_shift_tasks()
     elif all(st == 'не запланировано' for st in shift_tasks.values_list('st_status', flat=True)):
@@ -42,35 +45,92 @@ def tech_data_get(model_order_query: str, exel_file: str, excel_list: str):
         create_all_shift_tasks()
     else:
         tasks = []
-        errors = {
-            'changed_rows': {},
-            'deleted_rows': {},
-            'added_rows': {}
-        }
         allowed_fields = ('ws_number', 'norm_tech', 'draw_filename')
         for data in data_list:
-            updated_data = dict()
+            allowed_data = dict()
             for field in allowed_fields:
-                updated_data[field] = data.pop(field)
+                allowed_data[field] = data.pop(field)
             try:
-                shift_tasks = shift_tasks.exclude(excel_list_name=data['excel_list_name'])
                 task = ShiftTask.objects.get(**data)
-                for key, value in updated_data.items():
+                for key, value in allowed_data.items():
                     setattr(task, key, value)
                 tasks.append(task)
             except ShiftTask.DoesNotExist:
-                current_data = ShiftTask.objects.filter(excel_list_name=data['excel_list_name'])
-                row = int(data['excel_list_name'].split('-')[-1]) + 1
-                if current_data.exists():
-                    errors['changed_rows'].update({row: (current_data[0], data)})
-                else:
-                    data.update(updated_data)
-                    errors['added_rows'].update({row: data})
-        for task in shift_tasks:
-            row = int(task.excel_list_name.split('-')[-1]) + 1
-            errors['deleted_rows'].update({row: task})
-        ShiftTask.objects.bulk_update(tasks, allowed_fields)
-        return errors
+                is_uploaded = False
+    return is_uploaded
+
+
+
+
+        # changed_count = len(data_list) - len(shift_tasks)
+        # found_new_lines_count = 0
+        # for data in data_list:
+        #     input_row = data['excel_list_name']
+        #     updated_data = dict()
+        #     for field in allowed_fields:
+        #         updated_data[field] = data.pop(field)
+        #     try:
+        #
+        #         if changed_count > 0:
+        #             for add_row in range(1, changed_count + 2):
+        #                 try:
+        #                     task = ShiftTask.objects.get(**data)
+        #                     shift_tasks = shift_tasks.exclude(excel_list_name=data['excel_list_name'])
+        #                     break
+        #                 except ShiftTask.DoesNotExist:
+        #                     next_row_number = int(data['excel_list_name'].split('-')[1]) - 1
+        #                     data['excel_list_name'] = '-'.join((excel_list, str(next_row_number)))
+        #             else:
+        #                 found_new_lines_count += 1
+        #                 if found_new_lines_count <= changed_count:
+        #                     data['excel_list_name'] = input_row
+        #                     row = int(data['excel_list_name'].split('-')[-1]) + 1
+        #                     data.update(updated_data)
+        #                     errors['added_rows'].update({row: data})
+        #                 else:
+        #                     next_row_number = int(data['excel_list_name'].split('-')[1]) + changed_count - 1
+        #                     data['excel_list_name'] = '-'.join((excel_list, str(next_row_number)))
+        #                     shift_tasks = shift_tasks.exclude(excel_list_name=data['excel_list_name'])
+        #                     raise ShiftTask.DoesNotExist
+        #
+        #         elif changed_count < 0:
+        #             for add_row in range(1, abs(changed_count) + 2):
+        #                 try:
+        #                     task = ShiftTask.objects.get(**data)
+        #                     shift_tasks = shift_tasks.exclude(excel_list_name=data['excel_list_name'])
+        #                     break
+        #                 except ShiftTask.DoesNotExist:
+        #                     next_row_number = int(data['excel_list_name'].split('-')[1]) + 1
+        #                     data['excel_list_name'] = '-'.join((excel_list, str(next_row_number)))
+        #             else:
+        #                 next_row_number = int(data['excel_list_name'].split('-')[1]) - abs(changed_count)
+        #                 data['excel_list_name'] = '-'.join((excel_list, str(next_row_number)))
+        #                 shift_tasks = shift_tasks.exclude(excel_list_name=data['excel_list_name'])
+        #                 raise ShiftTask.DoesNotExist
+        #
+        #         else:
+        #             shift_tasks = shift_tasks.exclude(excel_list_name=data['excel_list_name'])
+        #             task = ShiftTask.objects.get(**data)
+        #             for key, value in updated_data.items():
+        #                 setattr(task, key, value)
+        #             tasks.append(task)
+        #
+        #     except ShiftTask.DoesNotExist:
+        #         current_data = ShiftTask.objects.filter(excel_list_name=data['excel_list_name'])
+        #         row = int(data['excel_list_name'].split('-')[-1]) + 1
+        #         if current_data.exists():
+        #             errors['changed_rows'].update({row: (current_data[0], data)})
+        #         else:
+        #             row = int(data['excel_list_name'].split('-')[-1]) + 1
+        #             data.update(updated_data)
+        #             errors['added_rows'].update({row: data})
+        #
+        # for task in shift_tasks:
+        #     row = int(task.excel_list_name.split('-')[-1]) + 1
+        #     errors['deleted_rows'].update({row: task})
+        #
+        # ShiftTask.objects.bulk_update(tasks, allowed_fields)
+        # return errors if any(len(value) > 0 for key, value in errors.items()) else None
 
 
 def get_excel_data(data: Dict, exel_file: str, excel_list: str) -> List:
--- omzit_terminal/tehnolog/views.py
+++ omzit_terminal/tehnolog/views.py
@@ -4,6 +4,8 @@ import asyncio
 
 from django.contrib.auth.decorators import login_required, permission_required
 from django.shortcuts import render, redirect
+
+from constructor.forms import QueryAnswer
 from .services.service_handlers import handle_uploaded_file
 from .services.tech_data_get import tech_data_get
 from .forms import GetTehDataForm, ChangeOrderModel, SendDrawBack
@@ -26,6 +28,7 @@ def tehnolog_wp(request):
     td_queries = (WorkshopSchedule.objects.values('model_order_query', 'query_prior', 'td_status')
                   .exclude(td_status='завершено'))
     change_model_query_form = ChangeOrderModel()
+    draw_files_upload_form = QueryAnswer()
     send_draw_back_form = SendDrawBack()
     alert = ''
     print(request.user.username[:8], 'tehnolog')
@@ -45,7 +48,9 @@ def tehnolog_wp(request):
                 get_teh_data_form.add_error(None, 'Файл должен быть .xlsx!')
                 context = {'get_teh_data_form': get_teh_data_form, 'td_queries': td_queries, 'alert': alert,
                            'change_model_query_form': change_model_query_form,
-                           'send_draw_back_form': send_draw_back_form}
+                           'send_draw_back_form': send_draw_back_form,
+                           'draw_files_upload_form': draw_files_upload_form
+                           }
                 return render(request, r"tehnolog/tehnolog.html", context=context)
             file_save_path = os.path.join(os.getcwd(), 'xlsx')
             os.makedirs(file_save_path, exist_ok=True)
@@ -55,26 +60,27 @@ def tehnolog_wp(request):
             print('filename=', filename, 'path=', file_save_path, 'xlsx=', xlsx_file)
             list_name = get_teh_data_form.cleaned_data['list_names']
             # вызов сервиса получения данных из xlsx
-            errors = tech_data_get(exel_file=xlsx_file, excel_list=list_name,
+            is_uploaded = tech_data_get(exel_file=xlsx_file, excel_list=list_name,
                           model_order_query=get_teh_data_form.cleaned_data['model_order_query'].model_order_query)
-            if errors:
-                context = {'errors': errors}
-                return render(request, r"tehnolog/tehnolog_errors.html", context=context)
-            alert = 'Загружено успешно!'
-            (WorkshopSchedule.objects.filter(model_order_query=get_teh_data_form.
-                                             cleaned_data['model_order_query'].model_order_query)
-             .update(tehnolog_query_td_fio=f'{request.user.first_name} {request.user.last_name}',
-                     td_status="утверждено",
-                     td_tehnolog_done_datetime=datetime.datetime.now()
-                     ))
-            # сообщение в группу
-            success_message = True
-            # try:
-            #
-            # except Exception as e:
-            #     print(f'Ошибка загрузки {filename}', e)
-            #     alert = f'Ошибка загрузки {filename}'
-            #     success_message = False
+            success_message = False
+            if is_uploaded:
+                alert = 'Загружено успешно!'
+                (WorkshopSchedule.objects.filter(model_order_query=get_teh_data_form.
+                                                 cleaned_data['model_order_query'].model_order_query)
+                 .update(tehnolog_query_td_fio=f'{request.user.first_name} {request.user.last_name}',
+                         td_status="утверждено",
+                         td_tehnolog_done_datetime=datetime.datetime.now()
+                         ))
+                # сообщение в группу
+                success_message = True
+            else:
+                alert = 'Ошибка загрузки! Изменены недопустимые поля, добавлены, удалены или перемещены строки!'
+               # try:
+                #
+                # except Exception as e:
+                #     print(f'Ошибка загрузки {filename}', e)
+                #     alert = f'Ошибка загрузки {filename}'
+                #     success_message = False
             if success_message:
                 success_group_message = (f"Загружен технологический процесс. Заказ-модель: "
                                          f"{get_teh_data_form.cleaned_data['model_order_query'].model_order_query} "
@@ -90,7 +96,9 @@ def tehnolog_wp(request):
         get_teh_data_form = GetTehDataForm()  # чистая форма для первого запуска
     context = {'get_teh_data_form': get_teh_data_form, 'td_queries': td_queries, 'alert': alert,
                'change_model_query_form': change_model_query_form,
-               'send_draw_back_form': send_draw_back_form}
+               'send_draw_back_form': send_draw_back_form,
+               'draw_files_upload_form': draw_files_upload_form
+               }
     return render(request, r"tehnolog/tehnolog.html", context=context)
 
 
@@ -138,19 +146,26 @@ def new_model_query(request):
     :return:
     """
     group_id = -908012934  # тг группа
-    # TODO переименовать папку
     if request.method == 'POST':
         change_model_query_form = ChangeOrderModel(request.POST)
         if change_model_query_form.is_valid():
             # модель_заказ
-            new_model_order_query = (f"{change_model_query_form.cleaned_data['order_query'].strip()}_"
-                                     f"{change_model_query_form.cleaned_data['model_query'].strip()}")
+            old_model_order_query = change_model_query_form.cleaned_data['model_order_query'].model_order_query
 
-            (WorkshopSchedule.objects.filter(model_order_query=change_model_query_form.
-                                             cleaned_data['model_order_query'].model_order_query)
-             .update(order=change_model_query_form.cleaned_data['order_query'].strip(),
-                     model_name=change_model_query_form.cleaned_data['order_query'].strip(),
+            new_order = change_model_query_form.cleaned_data['order_query'].strip()
+            new_model = change_model_query_form.cleaned_data['model_query'].strip()
+            new_model_order_query = f"{new_order}_{new_model}"
+
+            (WorkshopSchedule.objects.filter(model_order_query=old_model_order_query)
+             .update(order=new_order,
+                     model_name=new_model,
                      model_order_query=new_model_order_query))
+
+            # переименование папки
+            old_folder = os.path.join("C:/", "draws", old_model_order_query)
+            new_folder = os.path.join("C:/", "draws", new_model_order_query)
+            os.rename(old_folder, new_folder)
+
             # сообщение в группу
             success_group_message = (f"Заказ-модель переименован технологической службой в: "
                                      f"{new_model_order_query}. "
